1a51b7c54def3c874bbe434ccc019208a276edf3
batuqy <38671054+batuqy@users.noreply.github.com>
2022-07-16 18:41:06 +0300

    zarlar
    d4,d6,d8,d10,d12,d20

---
+++ gmtk2022/Assets/Scripts/scriptableDice.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="diceSide", menuName = "ScriptableObject/diceSide")]
+public class scriptableDice : ScriptableObject
+{
+    public int  sideValue;
+}
