960d815b8562905d5d72e5337a8eb3dc38a8f72f
Saidt <saidsoykan@outlook.com>
2022-07-16 22:48:53 +0300

    fix2

--- gmtk2022/Assets/Scripts/Map/Character.cs
+++ gmtk2022/Assets/Scripts/Map/Character.cs
@@ -6,6 +6,7 @@ public class Character : MonoBehaviour
 {
     public Transform line1,line2,line3,line4,line5,line6;
     Queue myQ = new Queue();
+    public GameObject character;
     void Start()
     {
         
@@ -14,22 +15,7 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            this.gameObject.transform.position = new Vector2(transform.position.x-1,transform.position.y);
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            this.gameObject.transform.position = new Vector2(transform.position.x, transform.position.y + 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            this.gameObject.transform.position = new Vector2(transform.position.x, transform.position.y - 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            this.gameObject.transform.position = new Vector2(transform.position.x + 1, transform.position.y);
-        }
+        
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
