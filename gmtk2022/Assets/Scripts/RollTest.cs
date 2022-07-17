2cefa6ca42eef4e865da031be702645131e3ec7c
batuqy <38671054+batuqy@users.noreply.github.com>
2022-07-17 20:54:05 +0300

    jh

--- gmtk2022/Assets/Scripts/RollTest.cs
+++ gmtk2022/Assets/Scripts/RollTest.cs
@@ -7,11 +7,14 @@ using System;
 public class RollTest : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI texts;
+    [SerializeField] TextMeshProUGUI[] textsOndice;
     [SerializeField] TextMeshProUGUI totalValue;
     public int sides;
     public int rollValue;
     public static int toplam;
     private static int x=20;
+    private static int kont;
+
     private float y;
     public GameObject enableGameObject;
     public void setRandomtoRollValue()
@@ -24,29 +27,72 @@ public class RollTest : MonoBehaviour
         
         if (toplam < 20 )
         {
-            
-            if (sides >x)
-            {
-                Debug.Log("yok");
-                texts.text = "X";
 
+            if (sides > x)
+            {
+                if (sides > x)
+                {
+                    for (int i = 6; i < 0; i--)
+                    {
+                        kont = toplam + sides;
+                        if (kont < 20)
+                        {
+                            textsOndice[i].text = "X";
+                        }
+                    }
+                }
             }
             else
             {
                 enableGameObject.SetActive(true);
                 setRandomtoRollValue();
-                x = x - rollValue; 
+                x = x - rollValue;
+
                 TotalValue();
                 UpdateText();
             }
         }
         else
         {
-            Debug.Log("yok");
-            texts.text = "X";
+            if (sides > x)
+            {
+                for (int i = 6; i < 0; i--)
+                {
+                    kont = toplam + sides;
+                    if (kont < 20)
+                    {
+                        textsOndice[i].text = "X";
+                    }
+                }
+            }
         }
-        
-        
+        if (sides > x)
+        {
+            for (int i = 6; i < 0; i--)
+            {
+                kont = toplam + sides;
+                if (kont < 20)
+                {
+                    textsOndice[i].text = "X";
+                }
+            }
+        }
+    }
+    public void FixedUpdate()
+    {
+        if (sides > x)
+        {
+            kont = toplam + sides;
+            if (kont < 21)
+            {
+                for (int i = 6; i < 0; i--)
+                {
+                    textsOndice[i].text = "X";
+                }
+            }
+                
+        }
+    
     }
     public int TotalValue()
     {
@@ -58,4 +104,44 @@ public class RollTest : MonoBehaviour
      texts.text =rollValue.ToString();
      totalValue.text = (toplam).ToString();
     }
+
+
+    public static float xD = 1;
+    public static float yD = 1;
+    private static float tapCounter = 0;
+    int[] xyz = new int[] { 4, 6, 8, 10, 12, 20 };
+    [SerializeField] TextMeshProUGUI textXd;
+    [SerializeField] TextMeshProUGUI textYd;
+
+    public void giveDirectionNumber()
+    {
+        if (tapCounter == 0)
+        {
+            xD = RollTest.toplam;
+            tapCounter++;
+            textXd.text = xD.ToString();
+            toplam = 0;
+            totalValue.text = (toplam).ToString();
+            x = 20;
+            for (int i = 0; i<6 ; i++)
+            {
+                    textsOndice[i].text = xyz[i].ToString();
+            }
+            
+        }
+        else if (tapCounter == 1)
+        {
+            tapCounter++;
+            yD = RollTest.toplam;
+            textYd.text = yD.ToString();
+            toplam = 0;
+            totalValue.text = (toplam).ToString();
+            x = 20;
+            for (int i = 0; i < 6; i++)
+            {
+                textsOndice[i].text = xyz[i].ToString();
+            }
+        }
+    }
 }
+
